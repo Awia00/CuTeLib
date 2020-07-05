c56603c0401d942d4a54bc404c426e39bc37c9be
Anders Wind <awia00@gmail.com>
2020-07-05 23:48:29 +0200

    NVCC eagerly compiles every function, and therefore the CPU access on GPU was changed to be at runtime.

--- CuTeLib/include/cute/defs.h
+++ CuTeLib/include/cute/defs.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 
-#if defined(__CUDA__ARCH__) || defined(__CUDACC__)
+#if defined(__CUDA_ARCH__) || defined(__CUDACC__)
 #define CUTE_NVCC_DEVICE __device__
 #define CUTE_NVCC_HOST __host__
 #else
@@ -14,9 +14,13 @@
 #define CUTE_DEV_HOST CUTE_NVCC_DEVICE CUTE_NVCC_HOST
 
 
-#ifdef __CUDA__ARCH__
-#define ENSURE_CORRECT_HARDWARE(MACRO_HARDWARE_VALUE) \
-    static_assert(MACRO_HARDWARE_VALUE == Hardware::GPU, "Trying to access cpu elements on the gpu")
+#ifdef __CUDA_ARCH__
+// I have not been successful in stopping the __device__ version being lazily compiled therefore we
+// can only do an assert here. The compiler should however be able to remove it as it will always
+// evaluate to true or false at compile time
+
+//"Trying to access cpu elements on the gpu"
+#define ENSURE_CORRECT_HARDWARE(MACRO_HARDWARE_VALUE) assert(MACRO_HARDWARE_VALUE == Hardware::GPU)
 #else
 #define ENSURE_CORRECT_HARDWARE(MACRO_HARDWARE_VALUE) \
     static_assert(MACRO_HARDWARE_VALUE == Hardware::CPU, "Trying to access gpu elements on the cpu")
